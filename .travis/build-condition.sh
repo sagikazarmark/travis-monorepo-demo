567c85ee852eb9df30783ca70a66d1df4bb200b4
Márk Sági-Kazár <mark.sagikazar@gmail.com>
2018-03-07 21:13:54 +0100

    Fix build condition script

--- .travis/build-condition.sh
+++ .travis/build-condition.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-if [[ -z $0 ]]; then
+if [[ -z $1 ]]; then
     echo "Commit range cannot be empty"
     exit 1
 fi
 
-if [[ -z $1 ]]; then
+if [[ -z $2 ]]; then
     echo "Change path cannot be empty"
     exit 1
 fi
 
-git diff --name-only $0 | sort -u | uniq | grep $1
+git diff --name-only $1 | sort -u | uniq | grep $2 > /dev/null
